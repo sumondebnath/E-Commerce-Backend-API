960437193e85bfe5970f4395ac27541342e98e9d
Sumon Debnath <debnathsumon1708@gmail.com>
2026-07-07 20:54:48 +0600

    PostgreSQL intregated in this project

--- E_Commerce_backend/settings.py
+++ E_Commerce_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import environ
 import os
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 
@@ -91,13 +92,20 @@ WSGI_APPLICATION = 'E_Commerce_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
