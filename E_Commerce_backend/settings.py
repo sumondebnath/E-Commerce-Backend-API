c9ace4e5202391cb862a5d68e771ee4278ef8ffc
Sumon Debnath <debnathsumon1708@gmail.com>
2026-08-09 13:28:09 +0600

    Replace postgresql to django default sqllite.

--- E_Commerce_backend/settings.py
+++ E_Commerce_backend/settings.py
@@ -102,13 +102,23 @@ WSGI_APPLICATION = 'E_Commerce_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+#         conn_max_age=600
+#     )
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
