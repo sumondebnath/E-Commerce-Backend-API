6bfc33cb5109e7489c244e22e543a9103f838326
Sumon Debnath <debnathsumon1708@gmail.com>
2026-08-09 14:07:27 +0600

    added supabase database

--- E_Commerce_backend/settings.py
+++ E_Commerce_backend/settings.py
@@ -101,14 +101,13 @@ WSGI_APPLICATION = 'E_Commerce_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 
 # DATABASES = {
