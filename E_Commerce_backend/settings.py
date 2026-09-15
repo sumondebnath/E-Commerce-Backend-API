63d11bddd1412720088eefeef2539998a4e12e85
Sumon Debnath <debnathsumon1708@gmail.com>
2026-09-16 02:26:14 +0600

    added cors allowed origins hardcorded

--- E_Commerce_backend/settings.py
+++ E_Commerce_backend/settings.py
@@ -219,12 +219,14 @@ CLOUDINARY_STORAGE = {
 }
 
 
+CORS_ALLOWED_ORIGINS=['https://shop-precom.netlify.app', 'https://api-ecommerce-46691.containers.snapdeploy.app']
 
-CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:5174',
-])
+
+# CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS', default=[
+#     'http://localhost:3000',
+#     'http://localhost:5173',
+#     'http://localhost:5174',
+# ])
 CORS_ALLOW_CREDENTIALS = True
 
 
